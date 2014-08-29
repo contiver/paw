5d56d30d3725cfc666adbe500bb70e3055e2b0b4
contiver <cristianontivero@gmail.com>
2014-08-29 18:07:21 -0300

    :)

--- http-cgi/productList2.c
+++ http-cgi/productList2.c
@@ -8,21 +8,24 @@ main(void){
     FILE *fp = fopen("products.txt", "r");
 
     printf("Server: Apache/2\n"
-           "Content-type: text/html\n\n"
-           "<html><body><h1>Lista de Productos</h1>");
-
-    printf("<h2>%s</h2>", qs);
+           "Content-type: text/html\n\n");
+    if (fp == NULL){
+        printf("<html><body><h1>No se encontro lista de productos</h1></body></html>");
+	return 0;
+    }
+    printf("<html><body><h1>Lista de Productos</h1>\n");
+    printf("<table><tr><td>Codigo");
 
     while( (c = fgetc(fp)) != EOF){
-        printf("<ul><li>%c",c);
+        printf("<tr><td>%c",c);
         while( (c = fgetc(fp)) != '\n'){
-            if (c == ',') printf("</li><li>");
+            if (c == ',') printf("</td><td>");
             else printf("%c",c);
         }
-        printf("</li></ul>");
+        printf("</td></tr>");
     }
 
-    printf("</body></html>");
+    printf("</table></body></html>");
 
     fclose(fp);
     return 0;
